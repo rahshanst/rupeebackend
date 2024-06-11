bbd36884b148fb8fc273fc37c6f8c90b0da3ed61
rahshan <rahshan@snowtint.com>
2024-06-11 10:27:21 +0530

    dev changes

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Add Azure remote repository
-git remote add azure https://onerupee-store-api-prod.scm.azurewebsites.net:443/onerupee-store-API-stage.git
+git remote add azure https://onerupee-store-api-stage.scm.azurewebsites.net:443/onerupee-store-API-stage.git
 
 # Switch to the prod branch
-git checkout prod
+git checkout dev
 
 # Pull the latest changes from the prod branch
-git pull origin prod
+git pull origin dev
 
 # Other commands for building and preparing your application for deployment
 # Example:
@@ -15,5 +15,4 @@ git pull origin prod
 # npm run build
 
 # Push the changes to the Azure remote repository (assuming prod branch is mapped to master on Azure)
-git push azure prod:master
-
+git push azure dev:master
