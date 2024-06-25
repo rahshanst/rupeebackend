2206082174340823e3a512b7be9bd202ce26f89d
rahshanst <92858998+rahshanst@users.noreply.github.com>
2024-06-25 13:14:46 +0530

    Update index.js api version to 1.1

--- api/apiconstants/index.js
+++ api/apiconstants/index.js
@@ -1,4 +1,4 @@
-const CHEGGOUT_URL= "https://restapi.cheggout.com/api/v1.2";
+const CHEGGOUT_URL= "https://restapi.cheggout.com/api/v1.1";
 
 const GEN_SWINKPAY_ORDER_ID = `${CHEGGOUT_URL}/GenerateSwinkPayOrderID`;
 const CHECK_SWINKPAY_STATUS = `${CHEGGOUT_URL}/CheckSwinkPayStatus`;
